a65174c10efea91d95aa23253bb0e12369abd5ad
Tom B. Parker <tombap8@hotmail.com>
2023-03-15 10:30:16 +0900

    xla

--- 팀프로젝트 행성 소개/합친거/sub/subjs/mercury.js
+++ 팀프로젝트 행성 소개/합친거/sub/subjs/mercury.js
@@ -5,15 +5,17 @@ window.addEventListener("DOMContentLoaded", loadFn);
 function loadFn() {
     // console.log("로딩 테스트");
 
-    /* 자동으로 이미지 바꾸기 */
-    // const mrcy_img = document.getElementsByClassName("mercury03")[0];
-    const mrcy_img = document.getElementById("mrcy_js");
-    // const mrcy_img = document.querySelector(".mercury03");
-    console.log(mrcy_img);
-    /* js 23458 10 12 */
-    // mrcy_img.style.cssText = "height: 50vh; background-image: url(../subimg/Mercury/Mercury02.jpg);background-repeat: no-repeat;background-position: center;background-size: cover;background-attachment: fixed;";
-    mrcy_img.style.backgroundImage = "url(../subimg/Mercury/Mercury03.jpg)";
-    // setTimeout(() => {
-    //     console.log(mrcy_img);
-    // }, 2000);
+    /*******************************
+    자동으로 이미지 바꾸기 
+    이미지 번호는 02 03 04 05 08 10 12 총 7개 -> 반복 시킨다. 
+    *******************************/
+    // const mrcy = document.querySelector(".mercury03");
+    // const mrcy = document.getElementsByClassName("mercury03")[0];
+    const mrcy = document.getElementById("mrcy_js");
+    console.log(mrcy);
+    
+    // mrcy.style.cssText = "height: 50vh; background-image: url(../subimg/Mercury/Mercury02.jpg);background-repeat: no-repeat;background-position: center;background-size: cover;background-attachment: fixed;";
+    // mrcy.style.backgroundImage = "url(../subimg/Mercury/Mercury03.jpg)";
+
+    // setTimeout(() => {}, 2000);
 } ////////// loadFn 함수 //////////
