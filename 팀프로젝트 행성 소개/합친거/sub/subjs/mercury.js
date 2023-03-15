041f0a3a3eca4569cf2d507199fb8df9fbb5a032
LSJ <lsjleo3788@naver.com>
2023-03-15 10:53:29 +0900

    팀프

--- 팀프로젝트 행성 소개/합친거/sub/subjs/mercury.js
+++ 팀프로젝트 행성 소개/합친거/sub/subjs/mercury.js
@@ -9,23 +9,17 @@ function loadFn() {
     자동으로 이미지 바꾸기 
     이미지 번호는 02 03 04 05 08 10 12 총 7개 -> 반복 시킨다. 
     *******************************/
-   let iseq = 0;
-   const inum = ['02','03','04','05','08','10','12'];
+    let iseq = 0;
+    const inum = ["02", "03", "04", "05", "08", "10", "12"];
     // const mrcy = document.querySelector(".mercury03");
-    // const mrcy = document.getElementsByClassName("mercury03")[0];
-    const mrcy = document.getElementById("mrcy_js");
+    const mrcy = document.getElementsByClassName("mercury03")[0];
+    // const mrcy = document.getElementById("mrcy_js");
     console.log(mrcy);
 
     setInterval(() => {
         iseq++;
-        if(iseq === inum.length) iseq = 0;
-        mrcy.style.cssText = `height: 50vh; background-image: url(./subimg/Mercury/Mercury${inum[iseq]}.jpg);background-repeat: no-repeat;background-position: center;background-size: cover;background-attachment: fixed;`;
-
-        mrcy.style.background = ""
-    }, 1000);
-    
-    ;
-    // mrcy.style.backgroundImage = "url(../subimg/Mercury/Mercury03.jpg)";
-
-    // setTimeout(() => {}, 2000);
+        if (iseq === inum.length) iseq = 0;
+        // mrcy.style.cssText = `height: 50vh; background-image: url(./subimg/Mercury/Mercury${inum[iseq]}.jpg);background-repeat: no-repeat;background-position: center;background-size: cover;background-attachment: fixed;`;
+        mrcy.style.backgroundImage = `url(./subimg/Mercury/Mercury${inum[iseq]}.jpg)`;
+    }, 3000);
 } ////////// loadFn 함수 //////////
