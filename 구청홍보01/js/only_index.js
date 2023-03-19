1e2bc5ae55cb540f44a276e84aa2d8152e7b27b0
LSJ <120539913+MOTORA22@users.noreply.github.com>
2023-03-19 14:21:48 +0900

    구청

--- 구청홍보01/js/only_index.js
+++ 구청홍보01/js/only_index.js
@@ -3,5 +3,5 @@ window.addEventListener("DOMContentLoaded", function () {
     const mas = document.querySelector("#oh");
     console.log(mas);
     
-    setTimeout(() => mas.style.overflow="visible", 3000);
+    setTimeout(() => mas.style.overflow="visible", 5000);
 });
