670c7aceed498b8b1690cae9e23c060820e9520b
LSJ <lsjleo3788@naver.com>
2023-03-20 17:03:43 +0900

    Update bg.js

--- 북구청_홍보_사이트/js/bg.js
+++ 북구청_홍보_사이트/js/bg.js
@@ -3,10 +3,10 @@ window.addEventListener("DOMContentLoaded", function () {
     const bgC = this.document.querySelector(".on");
     console.log(bgC);
 
-    let num = 1;
+    let num = 2;
     setInterval(()=>{
         bgC.style.background = `url(./img/abstract_bg${num<10 ? "0"+num:num}.jpg) no-repeat fixed center/cover`;
         num++;
         if(num === 19) num = 1;
-    },2000);
+    },2500);
 });
