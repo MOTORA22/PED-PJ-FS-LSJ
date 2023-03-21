9b085654ebcd5f0bb756e75e5d8bf46669946077
LSJ <lsjleo3788@naver.com>
2023-03-21 15:43:37 +0900

    구청

--- 북구청_홍보_사이트/js/bg.js
+++ 북구청_홍보_사이트/js/bg.js
@@ -3,10 +3,17 @@ window.addEventListener("DOMContentLoaded", function () {
     const bgC = this.document.querySelector(".on");
     console.log(bgC);
 
+    // let num = 2;
+    // setInterval(()=>{
+    //     bgC.style.background = `url(./img/abstract_bg${num<10 ? "0"+num:num}.jpg) no-repeat fixed center/cover`;
+    //     num++;
+    //     if(num === 19) num = 1;
+    // },6000);
+
     let num = 2;
     setInterval(()=>{
-        bgC.style.background = `url(./img/abstract_bg${num<10 ? "0"+num:num}.jpg) no-repeat fixed center/cover`;
+        bgC.style.background = `url(./img/bg0${num}.png) no-repeat fixed center/cover`;
         num++;
-        if(num === 19) num = 1;
+        if(num === 8) num = 1;
     },6000);
 });
